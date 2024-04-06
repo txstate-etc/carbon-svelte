c98ee3cdae8251359c70e0ca5d19bc25c5ad059c
Nick Wing <nwing@txstate.edu>
2024-04-06 08:49:14 -0500

    export tailwind config as a plugin, release 0.0.6

---
+++ src/lib/tailwind.js
@@ -0,0 +1,9 @@
+import plugin from 'tailwindcss/plugin.js'
+
+export default plugin(({ addBase, addComponents, addUtilities, addVariant }) => {
+
+}, {
+  theme: {
+
+  }
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+import carbonSveltePlugin from './src/lib/tailwind.js'
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [carbonSveltePlugin],
 }

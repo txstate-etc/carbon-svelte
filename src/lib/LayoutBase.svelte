ed35a9e2ae4cc2fdb54608d3bbca0e375906c88e
Nick Wing <nwing@txstate.edu>
2024-04-25 13:43:57 -0500

    make Roboto the default font

--- src/lib/LayoutBase.svelte
+++ src/lib/LayoutBase.svelte
@@ -2,11 +2,34 @@
   import { Theme } from 'carbon-components-svelte'
   import { Toasts } from '$lib/index.js'
   import 'carbon-components-svelte/css/all.css'
+  import '@fontsource/roboto/300.css'
+  import '@fontsource/roboto/400.css'
+  import '@fontsource/roboto/500.css'
+  import '@fontsource/roboto/700.css'
+  import '@fontsource/roboto/300-italic.css'
+  import '@fontsource/roboto/400-italic.css'
+  import '@fontsource/roboto/500-italic.css'
+  import '@fontsource/roboto/700-italic.css'
+  import '@fontsource/roboto-mono/400.css'
+  import '@fontsource/roboto-mono/600.css'
+  import '@fontsource/roboto-mono/400-italic.css'
+  import '@fontsource/roboto-mono/600-italic.css'
+  import '@fontsource/roboto-serif/400.css'
+  import '@fontsource/roboto-serif/500.css'
+  import '@fontsource/roboto-serif/600.css'
+  import '@fontsource/roboto-serif/400-italic.css'
+  import '@fontsource/roboto-serif/500-italic.css'
+  import '@fontsource/roboto-serif/600-italic.css'
   import './layoutbase.css'
 </script>
 
 <Theme tokens={{
-
+  'code-01-font-family': '"Roboto Mono", sans-serif',
+  'code-02-font-family': '"Roboto Mono", sans-serif',
+  'quotation-01-font-family': '"Roboto Serif", serif',
+  'quotation-02-font-family': '"Roboto Serif", serif',
+  'fluid-quotation-01-font-family': '"Roboto Serif", serif',
+  'fluid-quotation-02-font-family': '"Roboto Serif", serif'
 }}/>
 <Toasts />
 <slot/>
--- src/lib/tailwind.js
+++ src/lib/tailwind.js
@@ -4,6 +4,10 @@ export default plugin(({ addBase, addComponents, addUtilities, addVariant }) => 
 
 }, {
   theme: {
-
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      serif: ['Roboto Serif', 'serif'],
+      mono: ['Roboto Mono', 'monospace']
+    }
   }
 })

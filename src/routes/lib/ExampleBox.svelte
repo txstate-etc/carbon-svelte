bb72c60a587822a92ff46c3e133825be3a2075b7
Jeff Snider <jsnider@txstate.edu>
2024-08-01 09:50:44 -0500

    Refactored the examplebox quite a bit to demo in different settings

--- src/routes/lib/ExampleBox.svelte
+++ src/routes/lib/ExampleBox.svelte
@@ -1,19 +1,42 @@
-<div class="examplebox">
-  <slot />
-</div>
+<div class="exampleboxcontainer">
+  <div class="examplebox full">
+    <slot />
+  </div>
 
-<div class="examplebox small">
-  <slot />
+  <div class="examplebox half light">
+    <slot />
+  </div>
+
+  <div class="examplebox half dark">
+    <slot />
+  </div>
 </div>
 
 <style>
-  .examplebox {
-    border: 1px dashed black;
+  .exampleboxcontainer {
     margin: 0 1em 1em 1em;
+    padding: 1em;
+    background-color: #F8F8F8;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1em;
+    justify-content: center;
+  }
+  .examplebox {
+    border: 1px dashed #CCC;
+  }
+  .examplebox.full {
+    width: 100%;
+  }
+  .examplebox.half {
+    width: calc(50% - .5em);
   }
 
-  .examplebox.small {
-    max-width: 400px;
-    margin: auto;
+  .examplebox.light {
+    background-color: #FFF;
+  }
+
+  .examplebox.dark {
+    background-color: #222;
   }
 </style>

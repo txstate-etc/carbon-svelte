ebd4b3eba08ec418fb9c91cd8b6d8e27a2326dd3
Jeff Snider <jsnider@txstate.edu>
2024-08-07 17:32:43 -0500

    Use a single example and a transparency checkerboard

--- src/routes/lib/ExampleBox.svelte
+++ src/routes/lib/ExampleBox.svelte
@@ -1,29 +1,14 @@
-<script lang="ts">
-  export let single = false
-</script>
-
-
 <div class="exampleboxcontainer">
-  <div class="examplebox full {single ? '' : 'label'}">
+  <div class="examplebox">
     <slot />
   </div>
-
-  {#if !single}
-  <div class="examplebox half light label">
-    <slot />
-  </div>
-
-  <div class="examplebox half dark label">
-    <slot />
-  </div>
-  {/if}
 </div>
 
 <style>
   .exampleboxcontainer {
     margin: 0 1em 1em 1em;
     padding: 1em;
-    background-color: #EEE;
+    background-color: #F3F3F3;
     display: flex;
     flex-wrap: wrap;
     gap: 1em;
@@ -32,39 +17,27 @@
   .examplebox {
     border: 1px dashed #CCC;
     position: relative;
-  }
-  .examplebox.label {
-    margin-top: .8rem;
-  }
-  .examplebox.label:before {
-    font-size: .7rem;
-    line-height: 1rem;
-    padding: 0 .3rem;
-    height: 1rem;
-    position: absolute;
-    top: -1.05rem;
-    right: -1px;
-  }
-
-  .examplebox.full {
     width: 100%;
-  }
-  .examplebox.label.full:before {
-    content: 'Full Width, Inherit Background';
-  }
-  .examplebox.half {
-    width: calc(50% - .5em);
-  }
-  .examplebox.light {
-    background-color: #FFF;
-  }
-  .examplebox.light:before {
-    content: 'Half Width, Light Background';
-  }
-  .examplebox.dark {
-    background-color: #222;
-  }
-  .examplebox.dark:before {
-    content: 'Half Width, Dark Background';
+
+    background-image: repeating-linear-gradient(
+        45deg,
+        #F8F8F8 25%,
+        transparent 25%,
+        transparent 75%,
+        #F8F8F8 75%,
+        #F8F8F8
+      ),
+      repeating-linear-gradient(
+        45deg,
+        #F8F8F8 25%,
+        white 25%,
+        white 75%,
+        #F8F8F8 75%,
+        #F8F8F8
+      );
+    background-position:
+      0 0,
+      24px 24px;
+    background-size: 16px 16px;
   }
 </style>

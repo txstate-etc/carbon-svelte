1612e8e68d73315edbad0ab4fe7bdbf20f66848f
Jeff Snider <jsnider@txstate.edu>
2024-08-07 16:05:47 -0500

    Add labels on ExampleBox to explain the backgrounds

--- src/routes/lib/ExampleBox.svelte
+++ src/routes/lib/ExampleBox.svelte
@@ -4,16 +4,16 @@
 
 
 <div class="exampleboxcontainer">
-  <div class="examplebox full">
+  <div class="examplebox full {single ? '' : 'label'}">
     <slot />
   </div>
 
   {#if !single}
-  <div class="examplebox half light">
+  <div class="examplebox half light label">
     <slot />
   </div>
 
-  <div class="examplebox half dark">
+  <div class="examplebox half dark label">
     <slot />
   </div>
   {/if}
@@ -31,19 +31,40 @@
   }
   .examplebox {
     border: 1px dashed #CCC;
+    position: relative;
   }
+  .examplebox.label {
+    margin-top: .8rem;
+  }
+  .examplebox.label:before {
+    font-size: .7rem;
+    line-height: 1rem;
+    padding: 0 .3rem;
+    height: 1rem;
+    position: absolute;
+    top: -1.05rem;
+    right: -1px;
+  }
+
   .examplebox.full {
     width: 100%;
+  }
+  .examplebox.label.full:before {
+    content: 'Full Width, Inherit Background';
   }
   .examplebox.half {
     width: calc(50% - .5em);
   }
-
   .examplebox.light {
     background-color: #FFF;
   }
-
+  .examplebox.light:before {
+    content: 'Half Width, Light Background';
+  }
   .examplebox.dark {
     background-color: #222;
+  }
+  .examplebox.dark:before {
+    content: 'Half Width, Dark Background';
   }
 </style>

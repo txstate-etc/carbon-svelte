41ef935da3a24dc12414b6441462755051ea8a5b
Brian West <kny38@txstate.edu>
2024-09-05 11:38:09 -0500

    Moved custom tailwind colors to plugin

--- src/lib/tailwind.js
+++ src/lib/tailwind.js
@@ -8,6 +8,21 @@ export default plugin(({ addBase, addComponents, addUtilities, addVariant }) => 
       sans: ['Roboto', 'sans-serif'],
       serif: ['Roboto Serif', 'serif'],
       mono: ['Roboto Mono', 'monospace']
+    },
+    colors: {
+      tagyellow: {
+        50: '#fdf9ed',
+        100: '#f9eccc',
+        200: '#f3d690',
+        300: '#dfc27c',
+        400: '#ccae69',
+        500: '#b99a56',
+        600: '#a68744',
+        700: '#937532',
+        800: '#816320',
+        900: '#6f510c',
+        950: '#3f1709'
+      }
     }
   }
 })

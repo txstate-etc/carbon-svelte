234416caad23e5d6545fff5ffd7e1a3c2ccceeaf
Nick Wing <nwing@txstate.edu>
2024-09-05 13:48:34 -0500

    another try at sending custom colors in our tailwind plugin, release 1.2.20

--- src/lib/tailwind.js
+++ src/lib/tailwind.js
@@ -9,19 +9,21 @@ export default plugin(({ addBase, addComponents, addUtilities, addVariant }) => 
       serif: ['Roboto Serif', 'serif'],
       mono: ['Roboto Mono', 'monospace']
     },
-    colors: {
-      tagyellow: {
-        50: '#fdf9ed',
-        100: '#f9eccc',
-        200: '#f3d690',
-        300: '#dfc27c',
-        400: '#ccae69',
-        500: '#b99a56',
-        600: '#a68744',
-        700: '#937532',
-        800: '#816320',
-        900: '#6f510c',
-        950: '#3f1709'
+    extend: {
+      colors: {
+        tagyellow: {
+          50: '#fdf9ed',
+          100: '#f9eccc',
+          200: '#f3d690',
+          300: '#dfc27c',
+          400: '#ccae69',
+          500: '#b99a56',
+          600: '#a68744',
+          700: '#937532',
+          800: '#816320',
+          900: '#6f510c',
+          950: '#3f1709'
+        }
       }
     }
   }
